2a37470e61c4a7add89679c23b7bfc292ccba6df
eastend-street <ss_1322irf@icloud.com>
2019-11-22 15:27:27 -0800

    made header, transparent

--- src/components/commons/Header/Header.tsx
+++ src/components/commons/Header/Header.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import "./Header.scss"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      here is header
+      <div className="menu">
+        <ul>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">Team</NavLink>
+          </li>
+          <li>
+            <NavLink to="/press">Press</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
