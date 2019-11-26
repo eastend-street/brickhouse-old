5a132092a5c75d32e29060777e8ffc3df6a9850e
eastend-street <ss_1322irf@icloud.com>
2019-11-25 16:14:32 -0800

    added material ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,24 +4,26 @@ import "./Header.scss";
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
-      <div className="menu">
-        <ul>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">Team</NavLink>
-          </li>
-          <li>
-            <NavLink to="/press">Press</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="menu">
+          <ul>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Team</NavLink>
+            </li>
+            <li>
+              <NavLink to="/press">Press</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
   );
 };
 
