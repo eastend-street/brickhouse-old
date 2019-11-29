c143f849a6fb9a421c49e393ad94a382eee648db
eastend-street <ss_1322irf@icloud.com>
2019-11-29 16:53:32 -0700

    added contach layout

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Contact.scss";
 
+import { Grid } from "@material-ui/core";
+
 const Contact: React.FC = () => {
   return (
-      <div className="contact">Here is Contact page</div>
+    <div className="contact">
+      <h2>Contact us</h2>
+      <Grid container>
+        <Grid item xs={12} sm={5}>
+          here is contact form
+        </Grid>
+        <Grid item xs={12} sm={7}>
+          here is location
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
