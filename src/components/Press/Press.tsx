b9e507b741234d298e0e158c28548936dfdc9512
eastend-street <ss_1322irf@icloud.com>
2019-12-10 15:24:56 -0800

    made grid layout with lorem sample data. Needs to fix within 600px

--- src/components/Press/Press.tsx
+++ src/components/Press/Press.tsx
@@ -5,30 +5,43 @@ import PressJSON from "../../data/press.json";
 import "./Press.scss";
 
 type PressData = {
+  date: string;
   title: string;
   image_url: string;
   description: string;
 };
 
 const renderPress = () => {
-  return (
-    <>
-      <Grid item xs={3}>
-        <img src="" alt="aaaaaaaa" />
-      </Grid>
-      <Grid item xs={9}>
-        22222222
-      </Grid>
-    </>
-  );
+  const pressData: PressData[] = PressJSON;
+  return pressData.map((press: PressData, index) => {
+    return (
+      <React.Fragment key={index}>
+        <Grid item xs={3}>
+          <div className="press__left-section">
+            <img src={press.image_url} alt={press.title} />
+          </div>
+        </Grid>
+        <Grid item xs={9}>
+          <div className="press__right-section">
+            <div>
+              <h3>{press.title}</h3>
+              <div className="press__date">{press.date}</div>
+              <p className="press__description">{press.description}</p>
+            </div>
+          </div>
+        </Grid>
+      </React.Fragment>
+    );
+  });
 };
 
 const Press: React.FC = () => {
-  const pressData: PressData[] = PressJSON;
   return (
     <div className="press">
-      <h1>Press</h1>
-      <Grid container>{renderPress}</Grid>
+      <div className="press__inner">
+        <h1>Press</h1>
+        <Grid container>{renderPress()}</Grid>
+      </div>
     </div>
   );
 };
