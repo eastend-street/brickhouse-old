c3bdee783fc41ef84713d5bf931c660856eb6f52
eastend-street <ss_1322irf@icloud.com>
2019-12-14 13:24:34 -0700

    moved team to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,13 +4,15 @@ import "./Home.scss";
 import Top from "../Top/Top";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
-import Press from "../Press/Press"
+import Press from "../Press/Press";
+import Team from "../Team/Team";
 
 const Home: React.FC = () => {
   return (
     <div className="home">
       <Top />
       <About />
+      <Team />
       <Press />
       <Contact />
     </div>
